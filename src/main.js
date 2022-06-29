86edb4f3343ac66f02f7d4a10b05b0a2d175574e
literate <gaekoon@gmail.com>
2022-06-30 02:06:31 +0900

    기본 세팅 완료

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// @ts-check
+/* eslint-disable no-console */
+const express = require('express');
+
+const app = express();
+
+const PORT = 5000;
+
+app.use('/', (req, res) => {
+  res.send('Hello Express');
+});
+
+app.listen(PORT, () => {
+  console.log(`${PORT} is listening`);
+});
