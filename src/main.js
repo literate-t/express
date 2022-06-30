6acdd3b30cdcce9e4ea82b6d0963a463e37d7239
literate-t <gaekoon@gmail.com>
2022-06-30 20:34:35 +0900

    미들웨어 개념 파악하기

--- src/main.js
+++ src/main.js
@@ -1,13 +1,26 @@
 // @ts-check
 /* eslint-disable no-console */
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 const PORT = 5000;
+app.use(
+  "/",
+  (req, res, next) => {
+    console.log("Middleware 1-1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Middleware 1-2");
+    res.send("Hello Express 1");
+    next();
+  }
+);
 
-app.use('/', (req, res) => {
-  res.send('Hello Express');
+app.use((req, res, next) => {
+  console.log("Middleware 2");
+  //res.send("Hello Express 2");
 });
 
 app.listen(PORT, () => {
