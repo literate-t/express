a5e50a8ce1413bcfa0290aabb25edc059b203547
literate <gaekoon@gmail.com>
2022-07-03 20:23:36 +0900

    process imaga data with multer library

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ app.set('views', 'src/views');
 
 app.use('/users', userRouter);
 app.use('/public', express.static('src/public'));
+app.use('/uploads', express.static('uploads'));
 
 // error handling middleware
 app.use((err, req, res, next) => {
--- src/routers/users.js
+++ src/routers/users.js
@@ -2,15 +2,21 @@
 /* eslint-disalbe no-console */
 
 const express = require('express');
+const multer = require('multer');
+
+const upload = multer({ dest: 'uploads/' });
+
 const app = express();
 const router = express.Router();
 
 const USERS = {
   15: {
     nickname: 'Vik',
+    profileImage: undefined,
   },
   16: {
     nickname: 'Nic',
+    profileImage: undefined,
   },
 };
 
@@ -30,7 +36,7 @@ router.param('id', async (req, res, next, value) => {
     }
 
     // @ts-ignore
-    req.user = USERS[value];
+    req.user = user;
     next();
   } catch (err) {
     next(err); // 넣어줘야 함
@@ -43,12 +49,18 @@ router.get('/:id', (req, res, next) => {
     // @ts-ignore
     res.send(req.user);
   } else if (resMimeType === 'html') {
+    // @ts-ignore
+    console.log(req.user.profileImage);
     res.render('user-profile', {
       // @ts-ignore
       nickname: req.user.nickname,
+      userId: req.params.id,
+      // @ts-ignore
+      // profileImageURL: '/uploads/acd7847fc6d16adce1565dea3b4df67e',
+      profileImageURL: `/uploads/${req.user.profileImage}`,
     });
   }
-  console.log('router get id');
+  // console.log('router get id');
 });
 
 router.post('/', (req, res) => {
@@ -64,6 +76,15 @@ router.post('/:id/nickname', (req, res) => {
   user.nickname = nickname;
 
   res.send(`User nickname updated: ${nickname}`);
+});
+// name attribute of input
+router.post('/:id/profile', upload.single('profile'), (req, res, next) => {
+  // console.log(req.file);
+  // @ts-ignore
+  const { user } = req;
+  user.profileImage = req.file?.filename;
+
+  res.send(`User profile image uploaded:${user.profileImage}`);
 });
 
 module.exports = router;
